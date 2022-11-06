6ad50f4f02c03a59941c8e31766741dfc9917ba0
Gabriel Grinstein <gabriel.grinstein@gmail.com>
2022-11-06 02:41:32 -0800

    refactored Home Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useContext, useEffect } from "react";
 import CustomCard from "../components/Card";
 import moneyBag from "../assets/money-bag.png";
 import moneyPic from "../assets/home-pic.png";
@@ -6,73 +5,12 @@ import transactionPic from "../assets/transaction-vector.png";
 import mobilePayPic from "../assets/mobile-pay.png";
 import peoplePic from "../assets/busy-people.jpg";
 import { COLORS } from "../themes.js";
-import NavBar from "../components/NavBar";
-import { UserContext } from "../index";
-import { app } from "../firebase";
-import { getAuth } from "firebase/auth";
-import { QueryGetUserByEmail } from "../helper/queryMutationHelper";
 
 export default function Home() {
   console.count("---- HOME ----");
-  const ctx = useContext(UserContext);
-  const firebaseAuth = getAuth(app);
-  let id;
-  const [loggedIn, setLoggedIn] = useState(
-    firebaseAuth.currentUser ? true : false
-  );
-  const [email, setEmail] = useState("");
-
-  console.log("ctx", ctx);
-  ctx.user.id ? (id = ctx.user.id) : (id = "");
-
-  console.log("loggedIn state", loggedIn);
-  console.log("firebaseAuth.currentUser", firebaseAuth.currentUser);
-
-  useEffect(() => {
-    console.count("HOME useEffect");
-    firebaseAuth.onAuthStateChanged((userCredential) => {
-      console.log("HOME ONAUTHSTATECHANGED");
-      if (userCredential) {
-        console.log(userCredential.email);
-        if (!email) setEmail(userCredential.email);
-        // setLoggedIn(true);
-      } else {
-        id = "";
-        setLoggedIn(false);
-      }
-    });
-    if (firebaseAuth.currentUser !== null) setLoggedIn(true);
-  }, [email]);
-
-  // GET USER BY EMAIL GRAPHQL QUERY
-  try {
-    let { user } = QueryGetUserByEmail(email);
-
-    console.log("USER DATA", user);
-
-    if (user) id = user.id;
-  } catch (err) {
-    console.error("ERRORROROROROR", err.message);
-
-    if (err.message == "Data is null") {
-      console.error("DATA IS NULL");
-      // setShowPage(false);
-      //   return <PageNotFound id={paramId} />;
-    } else if (err.message == "Error getting User Data") {
-      return (
-        <h1 style={{ color: "red" }}>ERROR GETTING USER DATA: {err.message}</h1>
-      );
-    }
-  }
 
   return (
     <>
-      {/* {id ? (
-        <NavBar id={id} loggedIn={true} />
-      ) : (
-        <NavBar id={id} loggedIn={false} />
-      )} */}
-      {/* <NavBar id={id} /> */}
       <div className="page-wrapper">
         <span className="home-title-wrapper">
           <img
